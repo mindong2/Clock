fc3c90c8805ba2431cc2eb9712dc27573b6a7bef
mindong2 <qlsnaos12@naver.com>
2021-02-15 16:52:03 +0900

    toDoList 보수작업. (localstrage에 저장하는것과 delete기능 추가)

--- todo.js
+++ todo.js
@@ -4,17 +4,53 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
+function filterFn(toDo){
+    return toDo.id === 1;
+}
+
+let toDos = [];
+
+function deleteToDo(event){//밑에서 작성한 event
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);/*
+    target은 무엇을 누르는지 보여주고 
+    parentNode는 target의 부모노드*/
+    const cleanToDos = toDos.filter(function(toDo){
+        
+        return toDo.id !== parseInt(li.id);
+    });
+    //filter:toDos 배열에서 filterFn의 조건에 true인 것들만 return.
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+    /*toDos로 저장하려면 localStorage내의 
+    Value값은 String만 되기 때문에 
+    object를 string으로 바꿔주기위해 JSON.stringify사용*/
+}
 function paintToDo(text){
     const li = document.createElement("li"); 
     //HTML에서 얻는게 아니고 생성.
     const delBtn = document.createElement("button");
-    delBtn.innerText= "❌";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText= "❌";
+    delBtn.addEventListener('click', deleteToDo);
     span.innerText = text //submit function에서 온 값.
     li.appendChild(delBtn);
     li.appendChild(span); 
     //무언가를 그것의 부모요소의 자식목록 끝에 노드를 추가//
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId  //length는 0부터 시작하므로.
+    };
+    toDos.push(toDoObj); //toDos라는 배열안에 toDoObj를 push
+    saveToDos(); //push 한 후에 호출을 해줘야한다. 
 }
 
 function handleSubmit(event){
@@ -25,9 +61,13 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-     
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+     const parsedToDos = JSON.parse(loadedToDos);
+     // 위에서 object -> string 한것을 string -> object로 
+     parsedToDos.forEach(function(toDo){
+         paintToDo(toDo.text);
+     }); //forEach란 배열 안의 각각의 요소에 함수를 한번씩 실행  
     }
 }
 
