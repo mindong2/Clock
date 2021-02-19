7fcd130a3693d104d999e73299e35313f8bd3d46
mindong2 <qlsnaos12@naver.com>
2021-02-19 16:22:03 +0900

    Background images input and styling

---
+++ bg.js
@@ -0,0 +1,21 @@
+const body = document.querySelector(`body`);
+
+const IMG_NUMBER = 3;
+
+
+function paintImg(imgNumber){
+    const image = new Image();
+    image.src = `/images/landscape${imgNumber}.jpg`
+    image.classList.add(`bgImage`);
+    body.prepend(image);
+}
+
+    function genRandom(){
+        const number = Math.ceil(Math.random() * IMG_NUMBER )
+        return number ; 
+    }
+function init(){
+    const randomNumber = genRandom();
+    paintImg(randomNumber);
+}
+init()
